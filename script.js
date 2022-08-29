c2f3e784bd2ed7dce400ba5eda081e0c3a59ce05
PujaNeralwar <102819825+PujaNeralwar@users.noreply.github.com>
2022-08-29 20:06:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const canvas=document.getElementById("canvas")
+const ctx=canvas.getContext('2d')
+
+const input=document.getElementById("input");        
+const circleradius=230;
+
+
+// ctx.stroke()
+
+function show(){
+    const n=input.value;
+    if(n>2){
+ctx.clearRect(0,0,490,470)
+var angle=2*Math.PI/n;
+
+ctx.beginPath()
+ctx.moveTo(240+circleradius,240);
+for(let i=1;i<n;i++){
+ctx.lineTo(240+circleradius*Math.cos(angle*i),240-circleradius*Math.sin(angle*i));
+}
+
+ctx.closePath()
+ctx.strokeStyle='red';//`rgb(${parseInt(Math.random()*250)},${parseInt(Math.random()*250)},${parseInt(Math.random()*250)})`
+ctx.lineWidth='5';
+ctx.fillStyle='rgb(240, 66, 23)';
+ctx.fill();
+ctx.stroke()
+ctx.closePath()
+
+    }
+}
+
